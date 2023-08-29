2fe0cede5a2c2d416df52461e9a8a160c79a181f
effelima <tosta.fernando@gmail.com>
2023-08-29 17:04:24 -0300

    Added list of all lead times

--- controllers/DashboardControllers.js
+++ controllers/DashboardControllers.js
@@ -3,13 +3,16 @@ const IssueModel = require('../models/IssueModel')
 class DashboardControllers {
     get(req, res) {
         let issues = new IssueModel()
-        let kpi = {
+        let kpis = {
+            std : issues.getStdDeviationLeadTime(),
             mean : issues.getAverageLeadTime(),
             median : issues.getMedianLeadTime(),
-            //mode : issues.getModeLeadTime(),
-            frequency : issues.getModeFrequencyLeadTime()
+            mode_frequency : issues.getModeFrequencyLeadTime(),
+            sigma : issues.getSigma(),
+            issues_lt : issues.getAllLt()
         }
-        return res.json(kpi)
+
+        return res.json(kpis)
     }
 }
 
--- models/IssueModel.js
+++ models/IssueModel.js
@@ -5,25 +5,35 @@ class IssueModel {
 
     #model
     #lead_time
+    #average_lt
+    #median_lt
+    #mode_lt
+    #mode_freq_lt
+    #std_deviation
+    #sigma = {inf : 0, sup : 0}
 
     constructor() {
         this.#model = new IssuesToolsConnectors()
         this.#lead_time = this.#model.map(a => a.lead_time)
     }
 
+    getAllLt() {
+        return this.#lead_time
+    }
+
     getAverageLeadTime() {
-        let average = Mathjs.mean(this.#lead_time)
-        return average
+        this.#average_lt = Mathjs.mean(this.#lead_time)
+        return this.#average_lt
     }
 
     getMedianLeadTime() {
-        let median = Mathjs.median(this.#lead_time)
-        return median
+        this.#median_lt = Mathjs.median(this.#lead_time)
+        return this.#median_lt
     }
 
     getModeLeadTime() {
-        let mode = Mathjs.mode(this.#lead_time)
-        return mode
+        this.#mode_lt = Mathjs.mode(this.#lead_time)
+        return this.#mode_lt
     }
 
     getModeFrequencyLeadTime() {
@@ -41,8 +51,25 @@ class IssueModel {
                     frequency_mode[i].frequency++
             })
         })
-        return frequency_mode
+        
+        this.#mode_freq_lt = frequency_mode
+        return this.#mode_freq_lt
     }
+
+    getStdDeviationLeadTime() {
+        this.#std_deviation = Mathjs.std(this.#lead_time)
+        return this.#std_deviation
+    }
+
+    getSigma() {
+        // 3 is a value well know in six sigma
+        let sigma = this.#std_deviation * 3
+        this.#sigma.inf = this.#std_deviation - sigma
+        this.#sigma.sup = this.#std_deviation + sigma
+
+        return this.#sigma
+    }
+
 }
 
 module.exports = IssueModel
