8ae8bf9f67c128850621b1e5cdd405c7b918da3e
effelima <tosta.fernando@gmail.com>
2023-09-05 16:20:06 -0300

    Fix another variables names

--- src/use-cases/list-upper-lower-limits.js
+++ src/use-cases/list-upper-lower-limits.js
@@ -9,18 +9,17 @@ export default function makeUpperLowerLimits ({ leadTimeDb }) {
         const doneIssues = await leadTimeDb.findDoneIssuesByTeamId({
             teamId
         })
-        const weekLeadTime = doneIssues.map(( week ) => week.lt_weeks )
 
+        const weekLeadTime = doneIssues.map(( week ) => week.lt_weeks )
         const stdLeadTime = std(weekLeadTime)
         const meanLeadTime = mean(weekLeadTime)
         const countLeadTime = weekLeadTime.length
 
-        const leadTimeUpperLimit = meanLeadTime + 1.28 * (stdLeadTime / sqrt(countLeadTime))
-        const leadTimeDownLimit = meanLeadTime - 1.28 * (stdLeadTime / sqrt(countLeadTime))
-
-        return {
-            upperLimit : leadTimeUpperLimit,
-            lowerLimit : leadTimeDownLimit
+        const leadTimeLimits = {
+            upperLimit : meanLeadTime + 1.28 * (stdLeadTime / sqrt(countLeadTime)),
+            lowerLimit : meanLeadTime - 1.28 * (stdLeadTime / sqrt(countLeadTime))
         }
+
+        return leadTimeLimits
     }
   }
